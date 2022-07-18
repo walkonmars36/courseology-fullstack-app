40da89edd111eb25241868372b2cb3f892f40889
Mark Lawson <mark@walkonmars.co.uk>
2022-07-18 22:42:09 +0100

    created react frontend and spring boot backend

---
+++ backend/src/main/java/com/marklawson/courseology/courseology/CourseologyApplication.java
@@ -0,0 +1,13 @@
+package com.marklawson.courseology.courseology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseologyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseologyApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/marklawson/courseology/courseology/CourseologyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marklawson.courseology.courseology;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseologyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
