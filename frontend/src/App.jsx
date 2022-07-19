194e6c7a32ff2109175cbcb34cc785e7c8ee6852
Mark Lawson <mark@walkonmars.co.uk>
2022-07-19 19:28:26 +0100

    fetched course content

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,11 @@
 import logo from "./logo.svg";
-import {useEffect, useState} from "react";
-import "./App.css";
+import Courses from "./components/Courses/Courses";
+import "./App.scss";
 
 function App() {
-  const [greeting, setGreeting] = useState([]);
-
-  const getGreeting = async () => {
-    const response = await fetch("http://localhost:8080/hello");
-    const text = await response.text();
-    console.log(text);
-    setGreeting(text);
-  };
-
-  useEffect(() => {
-    getGreeting();
-  }, []);
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">{greeting}</h1>
-      </header>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
+    <div className="app__content">
+      <Courses />
     </div>
   );
 }
