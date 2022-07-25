95ab9f2b46bcc07b0ef43ea70c7e49352abf866c
Mark Lawson <mark@walkonmars.co.uk>
2022-07-25 11:55:00 +0100

    CourseDetails fetch working

--- frontend/src/components/CourseDetails/CourseDetails.jsx
+++ frontend/src/components/CourseDetails/CourseDetails.jsx
@@ -1,16 +1,30 @@
-import React from "react";
+import {useState, useEffect} from "react";
 import "./CourseDetails.scss";
 import {Link, useParams} from "react-router-dom";
 
-const CourseDetails = ({courses}) => {
-  const {id, title, summary} = courses;
+const CourseDetails = () => {
+  const [course, setCourse] = useState({});
 
   const {courseId} = useParams();
 
+  useEffect(() => {
+    const fetchCourse = async () => {
+      const response = await fetch(`http://localhost:8080/course/${courseId}`);
+      const json = await response.json();
+      console.log(json);
+      setCourse(json);
+    };
+    fetchCourse();
+  }, [courseId, setCourse]);
+
   return (
     <div className="course-details">
-      <h2 className="course-details__header">{title}</h2>
-      <p>{summary}</p>
+      {course && (
+        <div className="course-details__header">
+          <h1>{course.title}</h1>
+          <p>{course.summary}</p>
+        </div>
+      )}
 
       <Link to="/">Back to courses</Link>
     </div>
